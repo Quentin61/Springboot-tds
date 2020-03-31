7ec208322f48b47de91a6a643dd3192b9f402d9d
Quentin61 <45429939+Quentin61@users.noreply.github.com>
2020-03-31 15:39:42 +0200

    fix :
    - ajout par default de tous les scripts par default
    - suppression des import inutiles

--- src/main/java/s4/spring/td5/Controllers/ScriptController.java
+++ src/main/java/s4/spring/td5/Controllers/ScriptController.java
@@ -1,25 +1,22 @@
 package s4.spring.td5.Controllers;
 
 
-import com.sun.org.apache.xpath.internal.objects.XObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import io.github.jeemv.springboot.vuejs.VueJS;
 import io.github.jeemv.springboot.vuejs.utilities.Http;
-import io.github.jeemv.springboot.vuejs.utilities.JsArray;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import s4.spring.td5.entities.Script;
-import s4.spring.td5.entities.SearchObject;
-
-import javax.xml.ws.http.HTTPBinding;
-import java.util.List;
+import s4.spring.td5.repositories.ScriptRepository;
 
 @Controller
 public class ScriptController
 {
     @Autowired
     private VueJS vueJS;
+
+    @Autowired
+    private ScriptRepository scriptRepository;
 
     @ModelAttribute(name = "vue")
     private VueJS getVue() {
@@ -33,9 +30,10 @@ public class ScriptController
         vueJS.addDataRaw("choices", "[{text:'Title', value:'title'}, {text:'Description', value:'description'},{text:'Content', value:'content'}]");
         vueJS.addDataRaw("headers", "[{text:'Title', value:'title'},{text:'Description', value:'description'},{ text: 'Category', value: 'category'},{ text: 'Language', value: 'language'}]");
         vueJS.addMethod("searchBy","let self=this;"+Http.post(restUrl,(Object)"{'search': this.search, 'type': this.type}","self.scripts = response.data;"));
-        vueJS.addData("scripts",(Object)"this.scripts");
+        vueJS.addData("scripts",(Object)"scripts");
         vueJS.addData("search");
         vueJS.addData("type");
+        vueJS.addData("scripts",this.scriptRepository.findAll());
         vueJS.addWatcher("search", "this.searchBy();");
         return "vueJS/index";
     }
